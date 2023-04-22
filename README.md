1d5587946eda34d020aa66b4eb200dd8c53d64e4
Marc Ahlgrim <marc@onemarcfifty.com>
2023-04-22 13:57:16 +0200

    improved Server mode, updated readme
    Signed-off-by: Marc Ahlgrim <marc@onemarcfifty.com>

--- README.md
+++ README.md
@@ -1,17 +1,11 @@
-Graphical User Interface for iperf3
-===================================
+# Graphical User Interface for iperf3
 
-This is a python 3 program to give a graphical front end to local iperf3 tests
+![language](https://img.shields.io/github/languages/top/onemarcfifty/iperf3-gui)    ![License](https://img.shields.io/github/license/onemarcfifty/iperf3-gui)    ![Last Commit](https://img.shields.io/github/last-commit/onemarcfifty/iperf3-gui)     ![FileCount](https://img.shields.io/github/directory-file-count/onemarcfifty/iperf3-gui)    ![Stars](https://img.shields.io/github/stars/onemarcfifty/iperf3-gui)    ![Forks](https://img.shields.io/github/forks/onemarcfifty/iperf3-gui)
 
-## Revision History
-
-V1.3 - forked from NickWaterton and removed all internet related and ping stuff, ported to python3
-V1.2 - Added Yandex maps service due to impending google requirement for API key (both services now supported)
-V1.1 - Big re-write with new features, including google maps
-V1.0 : First release
-
+This is a graphical front end in python 3 for iperf3
 
 ## Introduction
+
 This program has the following features:
 * enter your own server ip/fqdn
 * ports configurable (and presets)
@@ -20,13 +14,16 @@ This program has the following features:
 * auto ranging
 * works on windows or linux
 * gives download and upload speeds
+* server and client mode supported
 
 ## Pre-Requisites
+
 You need iperf3 installed. It can be downloaded from here: https://iperf.fr/iperf-download.php
 
 
 ## Install
-First you need python 3.x installed
+
+Pre-requisite is an installation of python3.x
 
 now clone the repository from GitHub (obviously you need `git` installed)
 ```bash
@@ -40,58 +37,29 @@ No need to install anything, you can just run the program as is.
 run `./iperf.py -h` (or `python ./iperf.py -h` if you are on windows)
 
 ```
-usage: iperf.py [-h] [-I IPERF_EXEC] [-ip [IP_ADDRESS [IP_ADDRESS ...]]]
-                [-l LOCAL_IP] [-p PORT] [-r RANGE] [-R] [-m {OFF,Track,Peak}]
-                [-G] [-g GOOGLE_API_KEY] [-D] [-V] [-v]
+usage: iperf.py [-h] [-I IPERF_EXEC] [-ip [IP_ADDRESS ...]] [-p PORT] [-r RANGE] [-R] [-m {OFF,Track,Peak}]
+                [-D] [-S] [-V] [-v]
 
 Iperf3 GUI Network Speed Tester
 
 optional arguments:
   -h, --help            show this help message and exit
   -I IPERF_EXEC, --iperf_exec IPERF_EXEC
-                        location and name of iperf3 executable
-                        (default=D://utils//iperf3.exe)
-  -ip [IP_ADDRESS [IP_ADDRESS ...]], --ip_address [IP_ADDRESS [IP_ADDRESS ...]]
-                        default server address('s) can be a list
-                        (default=[u'192.168.100.119'])
+                        location and name of iperf3 executable (default=iperf3)
+  -ip [IP_ADDRESS ...], --ip_address [IP_ADDRESS ...]
+                        default server address('s) can be a list (default=[])
   -p PORT, --port PORT  server port (default=5201)
   -r RANGE, --range RANGE
                         range to start with in Mbps (default=10)
-  -R, --reset_range     Reset range to Default for Upload test (default =
-                        True)
+  -R, --reset_range     Reset range to Default for Upload test (default = True)
   -m {OFF,Track,Peak}, --max_mode {OFF,Track,Peak}
                         Show Peak Mode (default = Peak)
   -D, --debug           debug mode
+  -S, --server          run iperf in server mode
   -V, --verbose         print everything
   -v, --version         show program's version number and exit
 ```
 
-## Quick Start
-You need to know the pathname of your iperf3 executable, the default is `iperf3`, but you can use the `-I` option to specify the pathname
-for example on my Windows system I use
-```bash
-python .\iperf.py -I D:\utils\iperf3.exe
-```
-Because my `iperf3.exe` is in my `D:/utils directory`.
-Using Linux, the default usually works just fine (if iperf3 was installed using `apt-get` or is otherwise in your `PATH`, so you would use:
-```bash
-.\iperf.py
-```
+You can now run iperf3 tests from the GUI:
 
-You may need to be Administrator to use ping - it works fine for me as none-Administrator, but read the pyping page (if you are using it).
-
-To test on your local network, you will need another computer running another copy of iperf3 as as server:
-```bash
-.\iperf3 -s
-```
-You can then test your local network/wifi speeds against the new server you just started using it's ip address.
-
-If you select a remote server, you can test your actual internet speeds.
-
-You can enter a new server in the 'server' combobox, if it validates as an iperf3 server, the new server (plus maps etc) will be saved in the config.ini file, and automatically loaded the next time the program is started.
-
-You can add your own servers/ip addresses as a command line option `-ip` as a list of servers - these will not be saved in the config.ini file.
-Only remote servers are saved in the config.ini file, all local/private ip addresses are stored under your external ip address.
-If your external ip address cannot be determined, then maps are disabled. Also, if your external ip address changes, then new maps/distances will be stored in the config.ini file - in addition to the existing ones. So if you use a laptop, and move around, various maps/distances will be stored and reused depending on where you are. This is all automatic. If it goes wrong somehow, just delete config.ini and start again.
-
-That's it!
+![iperf_screenshot.png](iperf_screenshot.png)
--- iperf.py
+++ iperf.py
@@ -31,20 +31,27 @@ import psutil
 import sys
 import select
 
+# #######################################
+# The Mainframe class is the graphical
+# tk.Frame that is shown in the GUI
+# everything happens inside this class
+# #######################################
+
 class Mainframe(tk.Frame):
     
+    # The init method initializes all variables and
+    # draws the frame grid elements
     def __init__(self,master, arg=None, *args,**kwargs):
         tk.Frame.__init__(self, master, *args,**kwargs)
         
         self.ip_address = None      # ip address of current remote server
         self.arg = arg              # the command line arguments      
-        self.state = 'normal'
-        self.master = master
-        self.ip_info = {}
-        self.meter_size = 300       # meter is square
+        self.state = 'normal'       # the state can be 'normal' or 'disabled'
+        self.master = master        # back link to the tk.App
+        self.meter_size = 300       # Dimensions of the meter element
         self.no_response = 'No Response from iperf3 Server'
 
-        # in Server mode we create a list of local addresses that we can bind to
+        # in Server mode we create a list of local IP addresses that we can bind to
         if self.arg.server:
             self.server_list = []
             net_if_stats = psutil.net_if_stats()
@@ -83,26 +90,28 @@ class Mainframe(tk.Frame):
         self.min_range = 10
         self.resolution = 10
         self.ranges = [10,30,50,100,200,400,600,800,1000]
-        self.duration = tk.Variable()
-        self.threads = tk.Variable()
         self.server = tk.Variable()
         self.server_port = tk.Variable()
         self.range = tk.Variable()
         self.reset_range = tk.Variable()
-        self.threads.set('16')
         self.reset_range.set(self.arg.reset_range) 
 
-        self.msg_label = tk.Label(self, text="Message:")
-        self.msg_label.grid(row=0, sticky=tk.E)
-        
+        # Top level label used for (Error) Messages
+        tk.Label(self, text="Message:").grid(row=0, sticky=tk.E)
+        self.msg_label = tk.Label(self, anchor='w', width=60)
+        self.msg_label.grid(row=0, column=1, columnspan=3, sticky=tk.W)
+
+        # Download Label indicating the last Download speeds
         tk.Label(self, text="Download:").grid(row=2, sticky=tk.E)
         self.download_label = tk.Label(self, anchor='w', width=60)
         self.download_label.grid(row=2, column=1, columnspan=3, sticky=tk.W)
         
+        # Same for Upload Speed
         tk.Label(self, text="Upload:").grid(row=3, sticky=tk.E)
         self.upload_label = tk.Label(self, anchor='w', width=60)
         self.upload_label.grid(row=3, column=1, columnspan=3, sticky=tk.W)
         
+        # The meter gauge element
         self.meter = m.Meter(self,height = self.meter_size,width = self.meter_size)
         self.range.trace('w',self.updaterange)  #update range when value changes
         self.range.set(self.arg.range)
@@ -111,6 +120,8 @@ class Mainframe(tk.Frame):
  
         tk.Label(self, text="Server:").grid(row=6, sticky=tk.E)
 
+        # in Server mode, the dropdown list is read only
+        # in client mode you can enter a server name or address
         if self.arg.server:
             self.ipaddress= ttk.Combobox(self, 
                                          state="readonly",
@@ -125,8 +136,6 @@ class Mainframe(tk.Frame):
         self.ipaddress.current(0)
         self.ipaddress.grid(row=6, column=1, columnspan=2, sticky=tk.W)
 
-        # in server mode the combo is read only
-        
         self.port= ttk.Combobox(self, textvariable=self.server_port, values=self.port_list, width=4)
         if self.arg.port in self.port_list:
             self.port.current(self.port_list.index(self.arg.port))
@@ -136,12 +145,6 @@ class Mainframe(tk.Frame):
             self.port.current(0)
         self.port.grid(row=6, column=3, sticky=tk.W)
         
-        tk.Label(self, text="Progress:").grid(row=7, sticky=tk.E)
-        self.progress_bar = ttk.Progressbar(self
-        ,orient="horizontal"
-        ,length=325
-        , mode="determinate")
-        self.progress_bar.grid(row=7, column=1, columnspan=3, sticky=tk.W)
         
         tk.Label(self, text="Peak Mode:").grid(row=8, sticky=tk.E)
         self.max_mode_value = tk.StringVar()
@@ -160,36 +163,58 @@ class Mainframe(tk.Frame):
         tk.Label(self, text="Range:").grid(row=9, sticky=tk.W)
         self.range_box = ttk.Combobox(self, textvariable=self.range, values=self.ranges, width=4)
         self.range_box.grid(row=10, column=0, rowspan=2)
+
+        # in Server mode we don't define duration and number of threads        
+        if self.arg.server:
+            self.msg_label.config(text='*** SERVER MODE ***')
+        else:
+            tk.Label(self, text="Progress:").grid(row=7, sticky=tk.E)
+            self.progress_bar = ttk.Progressbar(self,
+                                                orient="horizontal",
+                                                length=325,
+                                                mode="determinate")
+            self.progress_bar.grid(row=7, column=1, columnspan=3, sticky=tk.W)
+            self.duration = tk.Variable()
+            self.threads = tk.Variable()
+            self.threads.set('16')
+            self.duration_scale = tk.Scale(self,
+                                           width = 15,
+                                           from_ = 10,
+                                           to = 30,
+                                           orient = tk.HORIZONTAL,
+                                           label='Test Duration',
+                                           variable=self.duration)
         
-        self.duration_scale = tk.Scale(self,width = 15 ,from_ = 10, to = 30
-        ,orient = tk.HORIZONTAL
-        ,label='Test Duration'
-        ,variable=self.duration)
-        self.duration_scale.grid(row=9, column=1, rowspan=2)
-        
-        self.threads_scale = tk.Scale(self,width = 15 ,from_ = 1, to = 30
-        ,orient = tk.HORIZONTAL
-        ,label='Threads'
-        ,variable=self.threads)
-        self.threads_scale.grid(row=9, column=2, rowspan=2)
+            self.duration_scale.grid(row=9, column=1, rowspan=2)
+            self.threads_scale = tk.Scale(self,
+                                          width = 15,
+                                          from_ = 1,
+                                          to = 30,
+                                          orient = tk.HORIZONTAL,
+                                          label='Threads',
+                                          variable=self.threads)
+            self.threads_scale.grid(row=9, column=2, rowspan=2)
 
         self.start_button = tk.Button(self,text = 'Start',width = 15,command = self.run_iperf)
         self.start_button.grid(row=9, column=3)
         self.quit = tk.Button(self,text = 'Quit',width = 15,command = self.quit)
         self.quit.grid(row=10, column=3)
         
+    # quit() is called when the Quit button is pressed
     def quit(self):
         print("Exit Program")
         self.done = True
         try:
-            self.p.terminate()
+            self.p.terminate() # try to terminate the running iperf process
             time.sleep(1)
         except AttributeError:
             pass
-        
         self.master.destroy()
         sys.exit(0)
-        
+
+    # set_control_state puts all visual elements on the form
+    # into 'disabled' mode (during iperf run) or
+    # 'normal' mode (in idle operations)        
     def set_control_state(self, state):
         self.state=state
         self.ipaddress.config(state=state)
@@ -197,8 +222,9 @@ class Mainframe(tk.Frame):
         self.max_mode.config(state=state)
         self.reset_range_box.config(state=state)
         self.range_box.config(state=state)
-        self.duration_scale.config(state=state)
-        self.threads_scale.config(state=state)
+        if not self.arg.server:
+            self.duration_scale.config(state=state)
+            self.threads_scale.config(state=state)
         
         # in disabled mode the start button becomes the stop button
         #self.start_button.config(state=state)
@@ -208,13 +234,19 @@ class Mainframe(tk.Frame):
             self.start_button.config(text="Start")
         self.update_idletasks()
     
+    # show_message displays a message in the top label
+    # on the frame
     def show_message(self, message, error=False):
+        print (f'M {message}')
         if error:
             self.msg_label.config(text=f'Error: {message}')
         else:
             self.msg_label.config(text='')
         self.update_idletasks()
 
+    # When the stop button has been clicked, we interrupt
+    # the running iperf process, reset the meter
+    # and return to idle ('normal') operation
     def stop_button_clicked(self):
         self.set_control_state('normal')
         try:
@@ -223,46 +255,61 @@ class Mainframe(tk.Frame):
             self.update_idletasks()
         except (tk.TclError, AttributeError):
             pass
-        
+
+    # run_iperf is called when the Start button is clicked.
+    # This is where we spawn the iperf3 process
     def run_iperf(self):
+
+        # in disabled mode, the Start button is actually
+        # the Stop button. In this case we call the 
+        # stop_button_clicked function and exit
         if self.state=='disabled':
             self.stop_button_clicked()
             return
+        
+        # verify if the Server Address is valid
         if self.server.get() == self.no_response:
             self.show_message('Please select/enter a vaild iperf3 server', True)
             self.update_idletasks()
             return
-        self.show_message('Testing', False)
+
+        # Reset the labels and disable the control elements
+        self.show_message('',False)     
         self.download_label.config(text='', width=60)
         self.upload_label.config(text='', width=60)
         self.meter.draw_bezel() #reset bezel to default
         self.set_control_state('disabled')
         self.update_idletasks()
-       
-#        self.update_idletasks()
         
-        if len(self.run_iperf3(upload=False)) != 0 and not self.done: #if we get some results (not an error)
-            if int(self.reset_range.get()) == 1:
-                self.range.set(self.arg.range)  #reset range to default
-            self.run_iperf3(upload=True)
-            if self.server.get() not in self.server_list:
-                self.server_list.append(self.server.get())
-                self.ipaddress.config(values=self.server_list)
-            try:
-                if not self.ip_info[self.ip_address].get('saved', False):
-                    self.write_config_file()
-            except KeyError:
-                pass
+        # now we actually call the iperf3 function
+        # in Client mode we do a Download first, then
+        # an upload. In Server mode we just wait
+        # for connections until the [Stop or Quit] button is pressed
+        if self.arg.server:
+            self.run_iperf3()
         else:
+            if len(self.run_iperf3(upload=False)) != 0 and not self.done: #if we get some results (not an error)
+                if int(self.reset_range.get()) == 1:
+                    self.range.set(self.arg.range)  #reset range to default
+                self.run_iperf3(upload=True)
             if self.server.get() not in self.server_list:
-                self.server.set(self.no_response)
+               self.server_list.append(self.server.get())
+               self.ipaddress.config(values=self.server_list)
+            else:
+                if self.server.get() not in self.server_list:
+                    self.server.set(self.no_response)
+
+        # After iperf3 terminates, we reset the frame to idle/normal
         self.set_control_state('normal')
         self.update_idletasks()
-        
+
+    # run_iperf spawns the iperf3 process
     def run_iperf3(self, upload=False):
         self.done = False
-        self.progress_bar["value"] = 0
-        self.progress_bar["maximum"] = int(self.duration.get())
+        # reset the progress bar and the meter
+        if not self.arg.server:
+            self.progress_bar["value"] = 0
+            self.progress_bar["maximum"] = int(self.duration.get())
         self.meter.max_val = 0
         self.meter.set(0)
         self.meter.show_max = self.max_options.index(self.max_mode_value.get()) #get index of item selected in max options
@@ -289,12 +336,16 @@ class Mainframe(tk.Frame):
                                                                              '' if upload else '-R')
         
         self.print("command: %s" % iperf_command)
-        message = 'Attempting connection - Please Wait'
-        if upload:
-            self.upload_label.config(text=message)
+        
+        if not self.arg.server:
+            message = 'Attempting connection - Please Wait'
+            if upload:
+                self.upload_label.config(text=message)
+            else:
+                self.download_label.config(text=message)   
+            self.update_idletasks()
         else:
-            self.download_label.config(text=message)   
-        self.update_idletasks()
+            self.msg_label.config(text='Waiting for connections')   
         try:
             self.p = subprocess.Popen(iperf_command.split(), stdout=subprocess.PIPE, stderr=subprocess.STDOUT,bufsize=1)
         except Exception as e:
@@ -302,15 +353,18 @@ class Mainframe(tk.Frame):
             print('Error in command: %s\r\n%s' % (iperf_command,e))
             return []
          
-        message = 'Testing'
-        if upload:
-            self.upload_label.config(text=message)
-        else:
-            self.download_label.config(text=message)   
+        if not self.arg.server:
+            message = 'Testing'
+            if upload:
+                self.upload_label.config(text=message)
+            else:
+                self.download_label.config(text=message)   
 
+        # once the process is spawned, we follow it in the progress function
         with fname:
             results_list = self.progress(fname if system_name().lower()=='windows' else self.p.stdout, upload)
-            
+
+        # when we are done, we terminate the process
         try:
             self.p.terminate()
             self.setmeter(0)
@@ -319,7 +373,8 @@ class Mainframe(tk.Frame):
             pass
         self.print('End of Test')
         return results_list
-        
+
+    # this function monitors the progress of the iperf3 process
     def progress(self,capture, upload):
         results_list = []
         units='bits/s'
@@ -357,7 +412,8 @@ class Mainframe(tk.Frame):
                 else:
 #                    if (int(self.threads.get()) > 1 and line.startswith('[SUM]')) or (int(self.threads.get()) == 1 and 'bits/sec' in line):
                     if (int(self.threads.get()) > 1 and '[SUM]' in line) or (int(self.threads.get()) == 1 and 'bits/sec' in line):
-                        self.progress_bar["value"] += 1
+                        if not self.arg.server:
+                            self.progress_bar["value"] += 1
                         speed = float(line.replace('[ ','[').replace('[ ','[').split()[5])
                         units = line.replace('[ ','[').replace('[ ','[').split()[6]
                         if 'receiver' in line:
@@ -405,22 +461,25 @@ class Mainframe(tk.Frame):
         value = int(value)
         #self.meter.set(value, True)
         self.meter.smooth_set(value, True)
-        
+
+# #######################################
+# The main Application class that is 
+# created from the main procedure
+# #######################################
+
 class App(tk.Tk):
     def __init__(self, arg):
         #super(App,self).__init__()
         tk.Tk.__init__(self)
         self.title('Iperf3 Network Speed Meter (V %s)' % __VERSION__)
         Mainframe(self, arg=arg).grid()
-        
-def global_imports(modulename,shortname = None, asfunction = False):
-    if shortname is None: 
-        shortname = modulename
-    if asfunction is False:
-        globals()[shortname] = __import__(modulename)
-    else:        
-        globals()[shortname] = eval(modulename + "." + shortname)
 
+# #######################################
+# The main function which takes the
+# Command line parameters and 
+# launches the TK app
+# #######################################
+        
 def main():
     import argparse
     max_mode_choices = ['OFF', 'Track', 'Peak']
@@ -441,7 +500,6 @@ def main():
     arg.max_mode_index = max_mode_choices.index(arg.max_mode)
     
     App(arg).mainloop()
-
     
 if __name__ == "__main__":
     main()
